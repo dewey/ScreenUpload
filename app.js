cca9f736b8f311222d2ea82c1a37747d06359625
dewey <mail@notmyhostna.me>
2014-10-17 23:12:30 +0200

    Remove logging

--- app.js
+++ app.js
@@ -15,15 +15,12 @@ var scpPort = '22';
 var scpRemotePath = '/var/www/img.notmyhostna.me/';
 var scpRemoteURL = 'https://img.notmyhostna.me/';
 // End of config
-// 
-console.log("Start ScreenUpload script")
 
 // Watch provided path from config for new files matching the screenshot default name.
 gaze('Screen\ Shot *.png', {
     cwd: screenshotDirectory
 }, function(err, watcher) {
     if (err) {
-        console.log("Error watching folder")
         console.log(err)
     } else {
         // Watch for 'added' event
@@ -33,7 +30,6 @@ gaze('Screen\ Shot *.png', {
             var imageNameNew = imageName + path.extname(filepath);
             var filepathNew = screenshotDirectoryArchive + imageNameNew;
             var imageRemoteURL = scpRemoteURL + imageNameNew;
-            console.log("Rename file")
             fs.renameSync(filepath, filepathNew)
 
             // Upload file
@@ -45,10 +41,8 @@ gaze('Screen\ Shot *.png', {
                 path: scpRemotePath
             }, function(err) {
                 if (err) {
-                    console.log("Uploading error")
                     console.log(err);
                 } else {
-                    console.log("Uploading successful")
                     // Copy to clipboard
                     copy(imageRemoteURL);
 
