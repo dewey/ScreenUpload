7e3f02d50e95229ce4621ff2f041686cfbb890de
dewey <mail@notmyhostna.me>
2014-10-18 19:50:46 +0200

    Read values from config

--- install.js
+++ install.js
@@ -1,15 +1,17 @@
-var Service = require('node-mac').Service;
+var Service = require('node-mac').Service,
+    config = require('./config');
 
-    // Create a new service object
-    var svc = new Service({
-      name:'ScreenUpload',
-      description: 'Install the ScreenUpload Deamon watching a defined path for new screenshots.',
-      script: 'app.js',
-      runAsUserAgent: true
-    });
+// Create a new service object
+var svc = new Service({
+    name: 'ScreenUpload',
+    description: 'Install the ScreenUpload Deamon watching a defined path for new screenshots.',
+    script: 'app.js',
+    runAsUserAgent: true,
+    cwd: config.install.cwd
+});
 
-    svc.on('install',function(){
-      svc.start();
-    });
+svc.on('install', function() {
+    svc.start();
+});
 
-    svc.install();
+svc.install();
